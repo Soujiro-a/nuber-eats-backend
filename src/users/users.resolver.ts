89c62053731599949a6a3db56e429bc77bcf70f9
Soujiro-a <qmaecx@naver.com>
2022-02-01 16:11:24 +0900

    Create Account mutation

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,13 +1,42 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import {
+  CreateAccountInput,
+  CreateAccountOutput,
+} from './dtos/create-account-dto';
 import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
 
 @Resolver(() => User)
 export class UsersResolver {
-  constructor(private readonly userService: UsersService) {}
+  constructor(private readonly usersService: UsersService) {}
 
   @Query(() => Boolean)
   hi() {
     return true;
+  }
+
+  @Mutation(() => CreateAccountOutput)
+  async createAccount(
+    @Args('input') createAccountInput: CreateAccountInput,
+  ): Promise<CreateAccountOutput> {
+    try {
+      const { ok, error } = await this.usersService.createAccount(
+        createAccountInput,
+      );
+      if (error) {
+        return {
+          error,
+          ok,
+        };
+      }
+      return {
+        ok,
+      };
+    } catch (error) {
+      return {
+        error,
+        ok: false,
+      };
+    }
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { CreateAccountInput } from './dtos/create-account-dto';
 import { User } from './entities/user.entity';
 
 @Injectable()
@@ -8,4 +9,28 @@ export class UsersService {
   constructor(
     @InjectRepository(User) private readonly users: Repository<User>,
   ) {}
+
+  async createAccount({
+    email,
+    password,
+    role,
+  }: CreateAccountInput): Promise<{ ok: boolean; error?: string }> {
+    try {
+      // 새 유저인지 체크하기
+      const exists = await this.users.findOne({ email });
+      // 이미 존재하는 이메일이라면
+      if (exists) {
+        // 에러 메시지 반환
+        return {
+          ok: false,
+          error: '해당 이메일을 가진 사용자가 이미 존재합니다.',
+        };
+      }
+      // 유저 만들기 & 비밀번호 해싱하기
+      await this.users.save(this.users.create({ email, password, role }));
+      return { ok: true };
+    } catch (error) {
+      return { ok: false, error: '유저를 생성할 수 없었습니다.' };
+    }
+  }
 }
