5a55fb7f26ac73c4ab336772caca5014f64cbbca
Soujiro-a <qmaecx@naver.com>
2022-02-02 11:56:12 +0900

    Create Login Mutation

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateAccountInput } from './dtos/create-account-dto';
+import { LoginInput } from './dtos/login.dto';
 import { User } from './entities/user.entity';
 
 @Injectable()
@@ -31,6 +32,40 @@ export class UsersService {
       return { ok: true };
     } catch (error) {
       return { ok: false, error: '유저를 생성할 수 없었습니다.' };
+    }
+  }
+
+  async login({
+    email,
+    password,
+  }: LoginInput): Promise<{ ok: boolean; error?: string; token?: string }> {
+    try {
+      // 입력받은 이메일을 가진 유저 찾기
+      const user = await this.users.findOne({ email });
+      if (!user) {
+        return {
+          ok: false,
+          error: '해당 이메일을 가진 유저를 찾을 수 없습니다.',
+        };
+      }
+      // 입력받은 비밀번호가 해당 유저의 비밀번호와 일치하는지 확인
+      const passwordCorrect = await user.checkPassword(password);
+      if (!passwordCorrect) {
+        return {
+          ok: false,
+          error: '비밀번호가 일치하지 않습니다.',
+        };
+      }
+      // JWT 만들고 user에게 주기(*)
+      return {
+        ok: true,
+        token: 'token test',
+      };
+    } catch (error) {
+      return {
+        ok: false,
+        error,
+      };
     }
   }
 }
