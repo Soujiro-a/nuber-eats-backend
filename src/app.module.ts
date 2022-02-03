cbf75d3084a4cdd0e7416086dc4d3c989bd246e4
Soujiro-a <qmaecx@naver.com>
2022-02-03 12:22:38 +0900

    Create and Configuration JWT Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { UsersModule } from './users/users.module';
 import { CommonModule } from './common/common.module';
 import * as Joi from 'joi';
 import { User } from './users/entities/user.entity';
+import { JwtModule } from './jwt/jwt.module';
 
 @Module({
   imports: [
@@ -23,6 +24,7 @@ import { User } from './users/entities/user.entity';
         DB_USERNAME: Joi.string().required(),
         DB_PASSWORD: Joi.string().required(),
         DB_NAME: Joi.string().required(),
+        PRIVATE_KEY: Joi.string().required(),
       }),
     }),
     TypeOrmModule.forRoot({
@@ -38,6 +40,9 @@ import { User } from './users/entities/user.entity';
     }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
+    }),
+    JwtModule.forRoot({
+      privateKey: process.env.PRIVATE_KEY,
     }),
     UsersModule,
     CommonModule,
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,14 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { sign } from 'jsonwebtoken';
 import { CreateAccountInput } from './dtos/create-account-dto';
 import { LoginInput } from './dtos/login.dto';
 import { User } from './entities/user.entity';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from 'src/jwt/jwt.service';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User) private readonly users: Repository<User>,
+    private readonly jwt: JwtService,
   ) {}
 
   async createAccount({
@@ -57,9 +61,10 @@ export class UsersService {
         };
       }
       // JWT 만들고 user에게 주기(*)
+      const token = this.jwt.sign({ id: user.id });
       return {
         ok: true,
-        token: 'token test',
+        token,
       };
     } catch (error) {
       return {
