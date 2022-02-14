a46057951b7c4a6b0d1db1b6fca72b843a5c7f0e
Soujiro-a <qmaecx@naver.com>
2022-02-14 14:23:13 +0900

    Fixed findById fundtion

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -93,13 +93,11 @@ export class UserService {
 
   async findById(id: number): Promise<UserProfileOutput> {
     try {
-      const user = await this.users.findOne({ id });
-      if (user) {
-        return {
-          ok: true,
-          user,
-        };
-      }
+      const user = await this.users.findOneOrFail({ id });
+      return {
+        ok: true,
+        user,
+      };
     } catch (error) {
       return {
         ok: false,
