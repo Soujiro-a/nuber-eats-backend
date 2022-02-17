37693c699da623f5f08b8696d1d290404843ae86
Soujiro-a <qmaecx@naver.com>
2022-02-17 12:14:56 +0900

    Create e2e Test createAccount function

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -4,6 +4,14 @@ import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
 import { getConnection } from 'typeorm';
 
+jest.mock('got', () => {
+  return {
+    post: jest.fn(),
+  };
+});
+
+const GRAPHQL_ENDPOINT = '/graphql';
+
 describe('UserModule (e2e)', () => {
   let app: INestApplication;
 
@@ -18,12 +26,62 @@ describe('UserModule (e2e)', () => {
 
   afterAll(async () => {
     await getConnection().dropDatabase();
-    await app.close();
+    app.close();
   });
 
-  it.todo('me');
+  describe('createAccount', () => {
+    const EMAIL_ACCOUNT = 'test@account.com';
+    it('계정을 생성합니다.', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+          mutation {
+          createAccount(input:{
+            email: "${EMAIL_ACCOUNT}",
+            password:"1234",
+            role:Owner
+          }) {
+            ok
+            error
+          }
+        }
+        `,
+        })
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.data.createAccount.ok).toBeTruthy();
+          expect(res.body.data.createAccount.error).toBeNull();
+        });
+    });
+
+    it('입력받은 이메일이 이미 존재한다면 계정 생성에 실패합니다.', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+          mutation {
+          createAccount(input:{
+            email: "${EMAIL_ACCOUNT}",
+            password:"1234",
+            role:Owner
+          }) {
+            ok
+            error
+          }
+        }
+        `,
+        })
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.data.createAccount.ok).toBeFalsy();
+          expect(res.body.data.createAccount.error).toEqual(expect.any(String));
+        });
+    });
+  });
   it.todo('userProfile');
   it.todo('login');
-  it.todo('editProfile');
+  it.todo('me');
   it.todo('verifyEmail');
+  it.todo('editProfile');
 });
