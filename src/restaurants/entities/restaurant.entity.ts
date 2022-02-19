8c2598c0131d235903e0cff446104683ca3e12e4
Soujiro-a <qmaecx@naver.com>
2022-02-19 13:49:44 +0900

    Fixed Restaurant Entity, and Config RestaurantModule in appModule

--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,28 +1,38 @@
-import { Field, ObjectType } from '@nestjs/graphql';
-import { IsBoolean, IsOptional, IsString, Length } from 'class-validator';
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Field, InputType, ObjectType } from '@nestjs/graphql';
+import { IsString, Length } from 'class-validator';
+import { CoreEntity } from 'src/common/entities/core.entity';
+import { User } from 'src/users/entities/user.entity';
+import { Column, Entity, ManyToOne } from 'typeorm';
+import { Category } from './category.entity';
 
+@InputType('RestaurantInputType', { isAbstract: true })
 @ObjectType()
 @Entity()
-export class Restaurant {
-  @Field((type) => Number)
-  @PrimaryGeneratedColumn()
-  id: number;
-
-  @Field((type) => String)
+export class Restaurant extends CoreEntity {
+  @Field(() => String)
   @Column()
   @IsString()
   @Length(5)
   name: string;
 
-  @Field((type) => Boolean, { defaultValue: true })
-  @Column({ default: true })
-  @IsOptional()
-  @IsBoolean()
-  isVegan: boolean;
-
-  @Field((type) => String)
+  @Field(() => String)
   @Column()
   @IsString()
   address: string;
+
+  @Field(() => String)
+  @Column()
+  @IsString()
+  coverImage: string;
+
+  @Field(() => Category, { nullable: true })
+  @ManyToOne(() => Category, (category) => category.restaurants, {
+    nullable: true,
+    onDelete: 'SET NULL',
+  })
+  category: Category;
+
+  @Field(() => User)
+  @ManyToOne(() => User, (user) => user.restaurants)
+  owner: User;
 }
