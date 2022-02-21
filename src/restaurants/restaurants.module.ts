acfee688880931d4d3233dd63d1076cd392482f2
Soujiro-a <qmaecx@naver.com>
2022-02-21 11:47:14 +0900

    Fixed createRestaurant logic

--- src/restaurants/restaurants.module.ts
+++ src/restaurants/restaurants.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Category } from './entities/category.entity';
 import { Restaurant } from './entities/restaurant.entity';
 import { RestaurantResolver } from './restaurants.resolver';
 import { RestaurantService } from './restaurants.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Restaurant])], // TypeOrmModule이 특정 feature를 import할 수 있게 해줌
+  imports: [TypeOrmModule.forFeature([Restaurant, Category])], // TypeOrmModule이 특정 feature를 import할 수 있게 해줌
   providers: [RestaurantResolver, RestaurantService],
 })
 export class RestaurantsModule {}
