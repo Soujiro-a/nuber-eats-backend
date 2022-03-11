a96f57fbaa70ba71d21777bb219c6a145439165d
Soujiro-a <qmaecx@naver.com>
2022-03-11 12:27:36 +0900

    Create createPayment logic

--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -1,11 +1,55 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Restaurant } from 'src/restaurants/entities/restaurant.entity';
+import { User } from 'src/users/entities/user.entity';
 import { Repository } from 'typeorm';
+import {
+  CreatePaymentInput,
+  CreatePaymentOutput,
+} from './dtos/create-payment.dto';
 import { Payment } from './entities/payment.entity';
 
 @Injectable()
 export class PaymentService {
   constructor(
     @InjectRepository(Payment) private readonly payments: Repository<Payment>,
+    @InjectRepository(Restaurant)
+    private readonly restaurants: Repository<Restaurant>,
   ) {}
+
+  async createPayment(
+    owner: User,
+    { transactionId, restaurantId }: CreatePaymentInput,
+  ): Promise<CreatePaymentOutput> {
+    try {
+      const restaurant = await this.restaurants.findOne(restaurantId);
+      if (!restaurant) {
+        return {
+          ok: false,
+          error: '음식점을 찾을 수 없습니다.',
+        };
+      }
+      if (restaurant.ownerId !== owner.id) {
+        return {
+          ok: false,
+          error: '음식점의 주인만 할 수 있는 작업입니다.',
+        };
+      }
+      await this.payments.save(
+        this.payments.create({
+          transactionId,
+          user: owner,
+          restaurant,
+        }),
+      );
+      return {
+        ok: true,
+      };
+    } catch {
+      return {
+        ok: false,
+        error: '결제를 진행할 수 없습니다.',
+      };
+    }
+  }
 }
